0742bee6d9b3d68b0a42474a074d0322b9cbdc05
Sly Automation <81003470+slyautomation@users.noreply.github.com>
2022-05-23 23:06:41 +1000

    Update osrs_item_prices.py

--- osrs_item_prices.py
+++ osrs_item_prices.py
@@ -5,13 +5,14 @@ import pandas as pd
 import webbrowser
 import numpy as np
 import os
+import dropbox
 from datetime import datetime
 
 # datetime object containing current date and time
 from pandas import json_normalize
 
 now = datetime.now()
-
+print('downloading data from prices.runescape.wiki...')
 print("now =", now)
 
 # dd/mm/YY H:M:S
@@ -31,6 +32,19 @@ pd.set_option('display.max_rows', None)
 # value display length is 100, the default is 50
 pd.set_option('max_colwidth', 200)
 
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        """upload a file to Dropbox using API v2
+        """
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+        return dbx
+    
 def ensure_dir():
     directory = os.path.dirname('csv')
     print(directory)
@@ -49,7 +63,9 @@ def call_http_prices():
     return stats
 
 # average over 12 hour window get data each hour over a week
-
+access_token = '' # add the access token from dropbox
+transferData = TransferData(access_token)
+ensure_dir()
 jsonData = call_http_prices()
 
 data = pd.DataFrame(jsonData)
@@ -92,3 +108,9 @@ data = format_data.drop(columns='timestamp').fillna(0)
 
 data.to_csv('csv/' + 'raw_data-' + datetime_time + '.csv', index=False)
 print('data saved to csv - data extracted as @', datetime_time)
+file_from = filename
+file_to = '/' + file_from
+# API v2
+db = transferData.upload_file(file_from, file_to)
+account db.users_get_current_account()
+print('data saved to dropbox account for:', account.name.given_name, account.name.surname)
